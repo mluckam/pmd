b568027ad93aefaf8d37bea9570afea38eb0cfc4
Matthew Luckam <mcl209@vt.edu>
2022-10-07 12:45:33 -0500

    first take a creating user visable constants for annotating supresswarning. Things to consider still     using pmd plugin does not expose the code.  For this reason, possbily creating a new submodule with the constants is the way to go for this.  That way a small jar of constants can be depended upon by the user instead of the entire pmd-java dep

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/security/SecurityRuleConstants.java
@@ -0,0 +1,9 @@
+package net.sourceforge.pmd.lang.java.rule.security;
+
+public class SecurityRuleConstants {
+
+	public static final String HARD_CODED_CRYPTO_KEY = "HardCodedCryptoKey";
+	public static final String INSECURE_CRYPTO_IV = "InsecureCryptoIv";
+
+	private SecurityRuleConstants() {}
+}
--- pmd-test/src/main/java/net/sourceforge/pmd/testframework/PmdRuleTst.java
+++ pmd-test/src/main/java/net/sourceforge/pmd/testframework/PmdRuleTst.java
@@ -4,10 +4,14 @@
 
 package net.sourceforge.pmd.testframework;
 
+import static org.junit.Assert.fail;
+
 import java.util.Collections;
 import java.util.List;
 
 import org.junit.runner.RunWith;
+
+import com.google.common.base.CaseFormat;
 
 import net.sourceforge.pmd.Rule;
 
@@ -27,6 +31,31 @@ public class PmdRuleTst extends RuleTst {
         String rulesetXml = "category/" + language + "/" + categoryName + ".xml";
 
         Rule rule = findRule(rulesetXml, getClass().getSimpleName().replaceFirst("Test$", ""));
+        hasRuleConstant(rule);
+
         return Collections.singletonList(rule);
+    }
+
+    private boolean hasRuleConstant(Rule rule) {
+        String ruleConstantName = CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, rule.getName());
+        String[] packages = rule.getClass().getPackage().getName().split("\\.");
+        String categoryName = packages[packages.length - 1];
+        String clazzString = rule.getClass().getPackage().getName() + "."
+                + CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_CAMEL, categoryName + "RuleConstants");
+
+        try {
+            Class<?> clazz = Class.forName(clazzString);
+            clazz.getField(ruleConstantName);
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+            fail(String.format("Class '%s' does not contain for constant '%s' for rule '%s'", clazzString,
+                    ruleConstantName, rule.getName()));
+            return false;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 }
